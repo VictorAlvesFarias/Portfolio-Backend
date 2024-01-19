82de589a3c8c5853726c58497bba65ebf36c207a
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-01-19 10:54:31 -0300

    Compile fixes

--- app/api/authorize/route.ts
+++ app/api/authorize/route.ts
@@ -14,15 +14,25 @@ export async function POST(req:NextApiRequest) {
     const currentPassword = await Password.find({ date: { $gte: maxDate  } })
 
     if(currentPassword[0]!=null&&currentPassword[0].code==accessKey) {
-        return {
-            authorized: true
-        }
+        return NextResponse.json({
+            message: "Authorized",
+            data: {
+                authorized: true
+            }
+        }, {
+            status: 200
+        });
     }
 
     else{
-        return {
-            authorized: false
-        }
+        return NextResponse.json({
+            message: "Unauthorized",
+            data: {
+                authorized: false
+            }
+        }, {
+            status: 401
+        });
     }
 
 }
