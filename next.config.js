1669aa6dc89ba8342f9e829b8fa466109f88029c
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-01-19 14:42:17 -0300

    Allow Cors fixes

--- next.config.js
+++ next.config.js
@@ -6,26 +6,13 @@ const nextConfig ={
         esmExternals: "loose", // <-- add this
         serverComponentsExternalPackages: ["mongoose"] // <-- and this
     },
-    headers: [{
-        source: "/api/(.*)",
-        headers: [{
-                "key": "Access-Control-Allow-Credentials",
-                "value": "true"
-            },
-            {
-                "key": "Access-Control-Allow-Origin",
-                "value": "*"
-            },
-            {
-                "key": "Access-Control-Allow-Methods",
-                "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-            },
-            {
-                "key": "Access-Control-Allow-Headers",
-                "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-            }
-        ]
-    }],
+    source: "/api/:path*",
+    headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    ],
     images: {
         dangerouslyAllowSVG: true,
         remotePatterns: [
