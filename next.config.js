73c44d9e8d4944795b9329d89724ba2ef81178f7
Victor Alves Farias <victoralvesfarias2004@gmail.com>
2024-12-02 10:16:32 -0300

    Logging in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,40 +1,45 @@
 /** 
  *@type {import('next').NextConfig} 
 */
-const nextConfig ={  
-    experimental: {
-        esmExternals: "loose", // <-- add this
-        serverComponentsExternalPackages: ["mongoose"] // <-- and this
-    },
-    async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,auth" },
-            ]
-          }
+const nextConfig = {
+  experimental: {
+    esmExternals: "loose", // <-- add this
+    serverComponentsExternalPackages: ["mongoose"] // <-- and this
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,auth" },
         ]
-    },
-    images: {
-        dangerouslyAllowSVG: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-                pathname: '**'
-            },
-            {
-                protocol: 'http',
-                hostname: '**',
-                pathname: '**'
-            }
-        ],
-}
+      }
+    ]
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        pathname: '**'
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+        pathname: '**'
+      }
+    ],
+  },
+  logging:{
+    fetches:{
+      fullUrl:true,
+    }
+  }
 }
 
 module.exports = nextConfig
